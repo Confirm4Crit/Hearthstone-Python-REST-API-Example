e5d53f9b0ec4324d6dc58babbd900c2a9e00ab8d
Miles Robson <miles_robson@yahoo.com>
2017-07-29 11:33:16 -0400

    Added redirect to card image.
    '/cardimage/<card_name>'

--- HSRestApi.py
+++ HSRestApi.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request,redirect
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
-from json import dumps
 from flask_jsonpify import jsonify
 
 db_connect = create_engine('sqlite:///cards.db')
 app = Flask(__name__)
 api = Api(app)
+artHSJSONURL = "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/"
 
 #select name from cards
 
@@ -32,9 +32,21 @@ class CardBasic(Resource):
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
+class CardImage(Resource):
+    def get(self,card_name):
+        conn = db_connect.connect()
+        query = conn.execute("select id from cards where lower(name) =(?)", (card_name,))
+        result = query.fetchone()
+        #TODO: Adjust for return value > 1 
+        idURLAppend = result[0] + '.png'
+        redirectURL = artHSJSONURL + idURLAppend
+        #TODO: Create own image DB, don't use HSJSON Art 
+        return redirect(redirectURL)
+
 api.add_resource(AllCardNames, '/allcardnames')
 api.add_resource(CardAllDetails, '/cardalldetails/<card_name>')
 api.add_resource(CardBasic, '/cardbasic/<card_name>')
+api.add_resource(CardImage, '/cardimage/<card_name>')
 
 if __name__ == '__main__':
      app.run(port='5000')
