34ae2cd376bd93c2c74b375c50109d5bb8c88bcb
Miles Robson <miles_robson@yahoo.com>
2017-07-29 11:50:37 -0400

    Adjusted variable names (PYLINT)

--- HSRestApi.py
+++ HSRestApi.py
@@ -3,16 +3,16 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from flask_jsonpify import jsonify
 
-db_connect = create_engine('sqlite:///cards.db')
-app = Flask(__name__)
-api = Api(app)
-artHSJSONURL = "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/"
+DB_CONNECT = create_engine('sqlite:///cards.db')
+APP = Flask(__name__)
+HS_API = Api(APP)
+CARD_ART_BASE_URL = "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/"
 
+#TODO: Change to DOCSTRING
 #select name from cards
-
 class AllCardNames(Resource):
     def get(self):
-        conn = db_connect.connect()
+        conn = DB_CONNECT.connect()
         query = conn.execute("select name from cards")
         return {'cards': [i[0] for i in query.cursor.fetchall()]}
 
@@ -20,33 +20,33 @@ class AllCardNames(Resource):
 #select specific card and return values /cardname/leeroy%20jenkins
 class CardAllDetails(Resource):
     def get(self, card_name):
-        conn = db_connect.connect()
+        conn = DB_CONNECT.connect()
         query = conn.execute("select * from cards where lower(name) =(?)", (card_name,))
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
 class CardBasic(Resource):
     def get(self, card_name):
-        conn = db_connect.connect()
+        conn = DB_CONNECT.connect()
         query = conn.execute("select name,type,text,rarity,cost,attack,health,cardClass,flavor from cards where lower(name) =(?)", (card_name,))
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
 class CardImage(Resource):
     def get(self,card_name):
-        conn = db_connect.connect()
+        conn = DB_CONNECT.connect()
         query = conn.execute("select id from cards where lower(name) =(?)", (card_name,))
         result = query.fetchone()
         #TODO: Adjust for return value > 1 
         idURLAppend = result[0] + '.png'
-        redirectURL = artHSJSONURL + idURLAppend
+        redirectURL = CARD_ART_BASE_URL + idURLAppend
         #TODO: Create own image DB, don't use HSJSON Art 
         return redirect(redirectURL)
 
-api.add_resource(AllCardNames, '/allcardnames')
-api.add_resource(CardAllDetails, '/cardalldetails/<card_name>')
-api.add_resource(CardBasic, '/cardbasic/<card_name>')
-api.add_resource(CardImage, '/cardimage/<card_name>')
+HS_API.add_resource(AllCardNames, '/allcardnames')
+HS_API.add_resource(CardAllDetails, '/cardalldetails/<card_name>')
+HS_API.add_resource(CardBasic, '/cardbasic/<card_name>')
+HS_API.add_resource(CardImage, '/cardimage/<card_name>')
 
 if __name__ == '__main__':
-     app.run(port='5000')
+     APP.run(port='5000')
